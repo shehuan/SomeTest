8b377c4dd84ff9fc5b14d6df241d79fa11530461
Othershe <shehuan320@163.com>
2018-03-20 23:10:44 +0800

    .

---
+++ app/src/main/java/com/shh/sometest/MainActivity.java
@@ -0,0 +1,35 @@
+package com.shh.sometest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tv_title)
+    TextView title;
+
+    @OnClick(R.id.bt_submit)
+    public void submit() {
+
+    }
+
+    private Unbinder unbinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        unbinder = ButterKnife.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        unbinder.unbind();
+        super.onDestroy();
+    }
+}
